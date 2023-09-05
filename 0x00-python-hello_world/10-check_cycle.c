6f10ea85ff0de09ad49b653fa016fd1987ac0aa3
himza25 <himza.of.theking95@gmail.com>
2023-09-04 20:26:58 -0700

    Task 10: Checking if a linked list has a cycle

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -2,26 +2,20 @@
 
 /**
  * check_cycle - checks if a singly linked list has a cycle in it.
- * @list: head of the linked list.
- * Return: 0 if there is no cycle, 1 if there is a cycle.
+ * @list: pointer to the start of the list
+ * Return: 0 if no cycle, 1 if there is a cycle
  */
 int check_cycle(listint_t *list)
 {
-	listint_t *tortoise, *hare;
+	listint_t *turtle = list;
+	listint_t *hare = list;
 
-	if (!list || !list->next)
-		return (0);
-
-	tortoise = list;
-	hare = list->next;
-
-	while (tortoise && hare && hare->next)
+	while (turtle && hare && hare->next)
 	{
-		if (tortoise == hare)
-			return (1);
-		tortoise = tortoise->next;
+		turtle = turtle->next;
 		hare = hare->next->next;
+		if (turtle == hare)
+			return (1);
 	}
-
 	return (0);
 }
