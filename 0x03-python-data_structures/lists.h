ae97db569557d2d8601bbff9ccc9b42410394ca5
himza25 <himza.of.theking95@gmail.com>
2023-09-12 14:28:38 -0700

    task 13

--- 0x03-python-data_structures/lists.h
+++ 0x03-python-data_structures/lists.h
@@ -1,3 +1,5 @@
+lists.h
+
 #ifndef LISTS_H
 #define LISTS_H
 
@@ -21,6 +23,5 @@ size_t print_listint(const listint_t *h);
 listint_t *add_nodeint_end(listint_t **head, const int n);
 void free_listint(listint_t *head);
 int is_palindrome(listint_t **head);
-void print_python_list_info(PyObject *p);
 
 #endif /* LISTS_H */
