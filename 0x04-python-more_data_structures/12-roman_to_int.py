1f86c632c1b846dff1c1c8ca4a4e1e3841414ce9
himza25 <himza.of.theking95@gmail.com>
2023-09-13 19:15:19 -0700

    Task 12: Roman to Integer

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,37 +1,20 @@
 #!/usr/bin/python3
 
 def roman_to_int(roman_string):
-    if type(roman_string) is not str or roman_string is None:
+    if not isinstance(roman_string, str) or roman_string is None:
         return 0
 
     roman_dict = {'I': 1, 'V': 5, 'X': 10, 'L': 50,
                   'C': 100, 'D': 500, 'M': 1000}
-
     total = 0
     prev_value = 0
 
-    for char in roman_string:
-        value = roman_dict[char]
-        if value > prev_value:
-            total += value - 2 * prev_value
+    for i in roman_string:
+        curr_value = roman_dict[i]
+        if curr_value > prev_value:
+            total += curr_value - 2 * prev_value
         else:
-            total += value
-        prev_value = value
+            total += curr_value
+        prev_value = curr_value
 
     return total
-
-if __name__ == "__main__":
-    roman_number = "X"
-    print("{} = {}".format(roman_number, roman_to_int(roman_number)))
-
-    roman_number = "VII"
-    print("{} = {}".format(roman_number, roman_to_int(roman_number)))
-
-    roman_number = "IX"
-    print("{} = {}".format(roman_number, roman_to_int(roman_number)))
-
-    roman_number = "LXXXVII"
-    print("{} = {}".format(roman_number, roman_to_int(roman_number)))
-
-    roman_number = "DCCVII"
-    print("{} = {}".format(roman_number, roman_to_int(roman_number)))
