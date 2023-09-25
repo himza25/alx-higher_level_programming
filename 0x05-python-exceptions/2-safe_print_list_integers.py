e11ca452e7c1978b9924b8fee309d36bcbbf14eb
himza25 <himza.of.theking95@gmail.com>
2023-09-25 13:39:35 -0700

    Add task 2: Print and count integers

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,14 +1,23 @@
 #!/usr/bin/python3
 
 def safe_print_list_integers(my_list=[], x=0):
-    num_printed = 0
+    """Prints first 'x' integers in a list.
+    
+    Args:
+        my_list: list of elements
+        x: number of elements to print from list
 
-    for idx in range(x):
+    Returns:
+        The actual number of integers printed
+    """
+    num_printed = 0  # Counter for actual number of integers printed
+    for i in range(x):
         try:
-            print("{:d}".format(my_list[idx]), end="")
-            num_printed += 1
-        except (ValueError, TypeError, IndexError):
-            continue
-            
-    print()
+            print("{:d}".format(my_list[i]), end="")
+            num_printed += 1  # Increment if successfully printed an integer
+        except IndexError:
+            raise  # Raising exception if index out of range
+        except (ValueError, TypeError):
+            pass  # Continue if element is not an integer
+    print("")
     return num_printed
