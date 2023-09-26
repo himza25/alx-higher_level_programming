4d49aba9a1592e607fcae23331346856594aeb79
himza25 <himza.of.theking95@gmail.com>
2023-09-25 18:23:22 -0700

    Added task: 103.python

--- 0x05-python-exceptions/103-python.c
+++ 0x05-python-exceptions/103-python.c
@@ -1,5 +1,9 @@
 #include <Python.h>
 
+void print_python_list(PyObject *p);
+void print_python_bytes(PyObject *p);
+void print_python_float(PyObject *p);
+
 /**
  * print_python_list - Prints some basic information about Python lists.
  * @p: Pointer to the Python list object.
