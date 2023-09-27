1af7394ff60a994e6af91554f19241cd8980e516
himza25 <himza.of.theking95@gmail.com>
2023-09-26 18:07:09 -0700

    Completed task 3: Added area method to Square class

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""
+This module defines a Square class that calculates the area of a square.
+It inherits from the square class with size validations.
+"""
+
+class Square:
+    """
+    This class represents a square.
+
+    Attributes:
+        __size (int): The size of the square, it must be an integer and >= 0.
+    """
+
+    def __init__(self, size=0):
+        """
+        Initialize a new Square.
+
+        Args:
+            size (int, optional): The size of the square.
+        """
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        """
+        Calculate and return the area of the square.
+
+        Returns:
+            int: The area of the square.
+        """
+        return self.__size ** 2
--- 0x06-python-classes/README.md
+++ 0x06-python-classes/README.md
@@ -1,3 +1,4 @@
 0. My first square: This task is about creating a simple Square class with no attributes or methods.
 1. Square with size: This task introduces a Square class with a private instance attribute 'size'.
 2. Size validation: The Square class now validates the size attribute to ensure it's an integer and >= 0.
+3. Area of a square: Added a method to the Square class that calculates and returns the area of the square.
