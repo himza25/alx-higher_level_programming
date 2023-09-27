0b11189da9416826ec92ba086835d24ddd1b9c84
himza25 <himza.of.theking95@gmail.com>
2023-09-26 17:55:15 -0700

    Completed task 0: My first square

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""
+This module defines a Square class for the purpose of creating Square objects.
+"""
+
+
+class Square:
+    """This class represents a Square with no attributes or methods."""
+    pass
+
---
+++ 0x06-python-classes/README.md
@@ -0,0 +1,1 @@
+0. My first square: This task is about creating a simple Square class with no attributes or methods.
