8cdbc773f62c7e75cb9017626adc2e1b3d3c311f
himza25 <himza.of.theking95@gmail.com>
2023-10-02 16:35:23 -0700

    Task 3: Extend Rectangle class to include a __str__ method for string representation.

---
+++ 0x08-python-more_classes/3-rectangle.py
@@ -0,0 +1,55 @@
+#!/usr/bin/python3
+"""Declare the Rectangle class"""
+
+
+class Rectangle:
+    """Defines a rectangle"""
+
+    def __init__(self, width=0, height=0):
+        """Initialize width and height attributes"""
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        """Retrieve the width attribute"""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """Set the width attribute"""
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """Retrieve the height attribute"""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """Set the height attribute"""
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    def area(self):
+        """Return the rectangle area"""
+        return self.__width * self.__height
+
+    def perimeter(self):
+        """Return the rectangle perimeter"""
+        if self.__width == 0 or self.__height == 0:
+            return 0
+        return 2 * (self.__width + self.__height)
+
+    def __str__(self):
+        """String representation of the rectangle"""
+        if self.__width == 0 or self.__height == 0:
+            return ""
+        return (("#" * self.__width + "\n") * self.__height)[:-1]
