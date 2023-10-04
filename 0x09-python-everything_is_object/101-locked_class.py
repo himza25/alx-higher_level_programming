ac4b2ad5e4f26b48421af0739e7662e6e27ed138
himza25 <himza.of.theking95@gmail.com>
2023-10-03 19:30:36 -0700

    Added advanced task: LockedClass with restricted attributes

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,3 +1,7 @@
 #!/usr/bin/python3
+"""Defines a class LockedClass with restricted attribute assignment."""
+
+
 class LockedClass:
+    """Prevents dynamic attribute creation except for 'first_name'."""
     __slots__ = ['first_name']
