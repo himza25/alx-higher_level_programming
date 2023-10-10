8ba7b1f7fd8b18b98bdb615b22f1ec386fae2b6e
himza25 <himza.of.theking95@gmail.com>
2023-10-09 17:19:52 -0700

    Completed task: MyList class that inherits from list

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,18 +1,13 @@
 #!/usr/bin/python3
-"""
-1. My list
-class:
-    that inherits from list:
-"""
-
+"""This module contains a class MyList that inherits from list."""
 
 class MyList(list):
+    """MyList class that inherits from list.
+    
+    Public instance method:
+        def print_sorted(self): prints the list, but sorted (ascending sort)
     """
-    declare a class MyList
-    that inherits from list
-    """
+    
     def print_sorted(self):
-        sorted_list = []
-        sorted_list += self
-        print(sorted(sorted_list))
-    pass
+        """Prints the list in ascending order."""
+        print(sorted(self))
