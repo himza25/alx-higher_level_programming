193cfb6bd0afda186aafcd72247d56eccb9e5af8
himza25 <himza.of.theking95@gmail.com>
2023-10-09 18:09:56 -0700

    Completed task: Geometry module with empty BaseGeometry class

---
+++ 0x0A-python-inheritance/5-base_geometry.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+"""
+This module contains an empty class named BaseGeometry.
+"""
+
+
+class BaseGeometry:
+    """
+    An empty class that serves as the base for other geometry-related classes.
+    """
+    pass
