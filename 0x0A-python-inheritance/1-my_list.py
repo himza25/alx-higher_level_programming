cc6fcff43badf17912f0e3883a0f1c73cfab4394
himza25 <himza.of.theking95@gmail.com>
2023-10-09 17:06:47 -0700

    Completed task: MyList class

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+"""
+This module contains a class MyList that inherits from list.
+The class has a public instance method to print the list in sorted order.
+"""
+
+
+class MyList(list):
+    """
+    MyList class that inherits from list.
+    """
+
+    def print_sorted(self):
+        """
+        Prints the list in ascending sorted order.
+        """
+        print(sorted(self))
--- 0x0A-python-inheritance/README.md
+++ 0x0A-python-inheritance/README.md
@@ -1,1 +1,2 @@
 This task is about creating a Python function that returns the list of available attributes and methods of an object.
+This task is about creating a Python class MyList that inherits from list and has a method to print the list in sorted order.
