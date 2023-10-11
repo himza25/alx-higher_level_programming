97fca3435dd9dec160cea373857dcea71efa972e
himza25 <himza.of.theking95@gmail.com>
2023-10-11 08:35:54 -0700

    Completed task 14: Log parsing

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -1,37 +1,40 @@
 #!/usr/bin/python3
 import sys
+import signal
 
 
 def print_stats(status_codes, total_size):
+    """Prints the statistics of status codes and total file size."""
     print("File size: {}".format(total_size))
     for code in sorted(status_codes.keys()):
         print("{}: {}".format(code, status_codes[code]))
 
 
+def signal_handler(sig, frame):
+    """Handles the keyboard interruption and prints statistics."""
+    print_stats(status_codes, total_size)
+    sys.exit(0)
+
+
 if __name__ == "__main__":
-    status_codes = {}
     total_size = 0
     line_count = 0
+    status_codes = {}
 
-    try:
-        for line in sys.stdin:
+    signal.signal(signal.SIGINT, signal_handler)
+
+    for line in sys.stdin:
+        try:
+            parts = line.strip().split(" ")
+            status_code = parts[-2]
+            file_size = int(parts[-1])
+
+            total_size += file_size
+            status_codes[status_code] = status_codes.get(status_code, 0) + 1
+
             line_count += 1
-            tokens = line.split()
-            if len(tokens) > 1:
-                status_code = tokens[-2]
-                file_size = int(tokens[-1])
-                total_size += file_size
-
-                if status_code in status_codes:
-                    status_codes[status_code] += 1
-                else:
-                    status_codes[status_code] = 1
-
             if line_count % 10 == 0:
                 print_stats(status_codes, total_size)
 
-    except KeyboardInterrupt:
-        print_stats(status_codes, total_size)
-        raise
-
-    print_stats(status_codes, total_size)
+        except Exception as e:
+            continue
