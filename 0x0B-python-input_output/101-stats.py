34e72eec99d182d2edd2d7db370d60ddaec13694
himza25 <himza.of.theking95@gmail.com>
2023-10-11 08:25:16 -0700

    Add log parsing script and update README

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -1,33 +1,46 @@
 #!/usr/bin/python3
-"""This script reads stdin line by line and computes metrics."""
-
 import sys
+import signal
 
-status_codes = {"200": 0, "301": 0, "400": 0, "401": 0,
-                "403": 0, "404": 0, "405": 0, "500": 0}
-total_size = 0
-line_count = 0
 
-try:
-    for line in sys.stdin:
-        line_count += 1
-        data = line.split()
-        total_size += int(data[-1])
-        status_code = data[-2]
-        if status_code in status_codes:
-            status_codes[status_code] += 1
+def signal_handler(sig, frame):
+    """Handle keyboard interruption and print statistics."""
+    print_statistics()
+    sys.exit(0)
 
-        if line_count % 10 == 0:
-            print("File size: {}".format(total_size))
-            for code, count in sorted(status_codes.items()):
-                if count > 0:
-                    print("{}: {}".format(code, count))
 
-except KeyboardInterrupt:
-    pass
-
-finally:
+def print_statistics():
+    """Print the statistics for the log."""
     print("File size: {}".format(total_size))
-    for code, count in sorted(status_codes.items()):
-        if count > 0:
-            print("{}: {}".format(code, count))
+    for code in sorted(status_codes.keys()):
+        if status_codes[code] > 0:
+            print("{}: {}".format(code, status_codes[code]))
+
+
+if __name__ == "__main__":
+    signal.signal(signal.SIGINT, signal_handler)
+    total_size = 0
+    line_count = 0
+    status_codes = {
+        '200': 0, '301': 0, '400': 0, '401': 0,
+        '403': 0, '404': 0, '405': 0, '500': 0
+    }
+
+    try:
+        for line in sys.stdin:
+            line_count += 1
+            parts = line.split()
+            if len(parts) > 1:
+                status_code = parts[-2]
+                file_size = int(parts[-1])
+                total_size += file_size
+
+                if status_code in status_codes:
+                    status_codes[status_code] += 1
+
+            if line_count % 10 == 0:
+                print_statistics()
+
+    except KeyboardInterrupt:
+        print_statistics()
+        sys.exit(0)
