55fb4bd386f3ad0efa8c6d93a52979c1edc2f9b6
himza25 <himza.of.theking95@gmail.com>
2023-10-11 08:56:01 -0700

    Task 14: Modified log parsing script

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -1,19 +1,19 @@
 #!/usr/bin/python3
-"""Reads from standard input and computes metrics.
+"""Log Parsing Script.
 
-After every ten lines or the input of a keyboard interruption (CTRL + C),
-prints the following statistics:
-    - Total file size up to that point.
-    - Count of read status codes up to that point.
+This script reads lines from the standard input and calculates metrics.
+It prints the following statistics after every 10 lines or when interrupted by CTRL + C:
+    - Cumulative file size.
+    - Frequency of each HTTP status code encountered.
 """
 
 
 def print_stats(size, status_codes):
-    """Print accumulated metrics.
+    """Display the calculated metrics.
 
     Args:
-        size (int): The accumulated read file size.
-        status_codes (dict): The accumulated count of status codes.
+        size (int): The total file size read so far.
+        status_codes (dict): A dictionary containing the count of each status code.
     """
     print("File size: {}".format(size))
     for key in sorted(status_codes):
