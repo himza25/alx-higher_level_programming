89370c116eaf2379ac012635cf0fb3cd968250ca
himza25 <himza.of.theking95@gmail.com>
2023-10-11 07:45:38 -0700

    Completed task 0: Read file

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+This module contains a function that reads a text file (UTF8)
+and prints it to stdout.
+"""
+
+
+def read_file(filename=""):
+    """
+    Reads a text file (UTF8) and prints it to stdout.
+    """
+    with open(filename, 'r', encoding='utf-8') as f:
+        print(f.read(), end="")
---
+++ 0x0B-python-input_output/README.md
@@ -0,0 +1,1 @@
+This task involves creating a Python function that reads a text file (UTF8) and prints its content to stdout.
