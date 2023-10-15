a95e662f56d2e71c01b3e14f16595dec1fdbbbf2
himza25 <himza.of.theking95@gmail.com>
2023-10-15 02:52:29 -0700

    Implemented Rectangle class with validation, area calculation, dicrionary and display methods

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -105,3 +105,18 @@ class Rectangle(Base):
             for key, value in kwargs.items():
                 if key in attributes:
                     setattr(self, key, value)
+
+    def to_dictionary(self):
+        """
+        Returns the dictionary representation of a Rectangle instance.
+
+        Returns:
+            dict: A dictionary containing 'id', 'width', 'height', 'x', 'y'
+        """
+        return {
+            'id': self.id,
+            'width': self.__width,
+            'height': self.__height,
+            'x': self.__x,
+            'y': self.__y
+        }
