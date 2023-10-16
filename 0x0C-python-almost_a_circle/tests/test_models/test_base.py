5ef50879fcce0559ba98037acf4994493e93e100
himza25 <himza.of.theking95@gmail.com>
2023-10-16 11:37:20 -0700

    Completed task with classes and test cases

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -1,33 +1,30 @@
 #!/usr/bin/python3
-"""This module defines test cases for the Base class"""
-
+""" 0-unittest """
 import unittest
 from models.base import Base
 
 
 class TestBase(unittest.TestCase):
-    """Test cases for the Base class"""
 
-    def test_init(self):
-        """Test initialization of the Base class"""
+    def test_int_none(self):
         b1 = Base()
         self.assertEqual(b1.id, 1)
+
+        self.assertEqual(b1.id, 1)
+
+        self.assertEqual(Base().id, 2)
+
         b2 = Base()
-        self.assertEqual(b2.id, 2)
-        b3 = Base(12)
-        self.assertEqual(b3.id, 12)
+        self.assertEqual(b2.id, 3)
 
-    def test_to_json_string(self):
-        """Test the to_json_string method"""
-        list_dicts = [{"id": 1}, {"id": 2}]
-        expected = '[{"id": 1}, {"id": 2}]'
-        self.assertEqual(Base.to_json_string(list_dicts), expected)
+        b3 = Base()
+        self.assertEqual(b3.id, 4)
 
-    def test_from_json_string(self):
-        """Test the from_json_string method"""
-        json_str = '[{"id": 1}, {"id": 2}]'
-        expected = [{"id": 1}, {"id": 2}]
-        self.assertEqual(Base.from_json_string(json_str), expected)
+        b4 = Base(66)
+        self.assertEqual(b4.id, 66)
+
+        b5 = Base()
+        self.assertEqual(b5.id, 5)
 
 
 if __name__ == '__main__':
