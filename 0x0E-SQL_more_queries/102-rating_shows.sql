c682aa360b801c994b6b20c420139cda5479dc9c
himza25 <himza.of.theking95@gmail.com>
2023-11-16 05:10:41 -0800

    from task 16 to 20

--- 0x0E-SQL_more_queries/102-rating_shows.sql
+++ 0x0E-SQL_more_queries/102-rating_shows.sql
@@ -1,6 +1,7 @@
 -- List all shows by their rating from hbtn_0d_tvshows_rate
-SELECT tv_shows.title, SUM(rating) AS rating
-FROM tv_shows_rate
-JOIN tv_shows ON tv_shows.id = tv_shows_rate.show_id
-GROUP BY tv_shows.title
+SELECT title, SUM(rate) AS rating
+FROM tv_shows
+INNER JOIN tv_show_ratings
+ON tv_shows.id = tv_show_ratings.show_id
+GROUP BY title
 ORDER BY rating DESC;
