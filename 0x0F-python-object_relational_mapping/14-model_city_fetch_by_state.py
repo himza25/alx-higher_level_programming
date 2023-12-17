46868f0187baf73dd3fa0df72acec705b0cbe1af
himza25 <himza.of.theking95@gmail.com>
2023-12-17 13:33:33 -0800

    from task 13 to task 14

--- 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -2,8 +2,9 @@
 """
 Prints all City objects from the database hbtn_0e_14_usa
 """
+
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker, joinedload
+from sqlalchemy.orm import sessionmaker
 from model_state import Base, State
 from model_city import City
 import sys
@@ -16,6 +17,7 @@ if __name__ == "__main__":
     )
     Session = sessionmaker(bind=engine)
     session = Session()
-    for city in session.query(City).join(State).order_by(City.id).all():
-        print("{}: ({}) {}".format(city.state.name, city.id, city.name))
+    query = session.query(City, State).join(State).order_by(City.id).all()
+    for city, state in query:
+        print("{}: ({}) {}".format(state.name, city.id, city.name))
     session.close()
--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -2,6 +2,7 @@
 """
 Definition of the City class
 """
+
 from sqlalchemy import Column, Integer, String, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
 from model_state import Base
@@ -12,9 +13,6 @@ class City(Base):
     The City class:
     - inherits from Base
     - links to the MySQL table cities
-    - id: auto-generated, unique integer, primary key
-    - name: string of 128 characters, cannot be null
-    - state_id: integer, cannot be null, foreign key to states.id
     """
     __tablename__ = 'cities'
     id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
