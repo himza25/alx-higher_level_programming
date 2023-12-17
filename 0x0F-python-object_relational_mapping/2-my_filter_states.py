4d2ee4d694902455a7d0388555099cb63cd1c47c
himza25 <himza.of.theking95@gmail.com>
2023-12-17 12:52:51 -0800

    from task 1 to Task 3

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,28 +1,33 @@
 #!/usr/bin/python3
 """
-This module contains a script that takes in an argument and displays all values
-in the states table of hbtn_0e_0_usa where name matches the argument.
+This script takes in an argument and displays all values in the states table
+of hbtn_0e_0_usa where name matches the argument.
 """
 
 import MySQLdb
 import sys
 
-if __name__ == "__main__":
+def main():
     # Connect to the database
-    conn = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
-                           passwd=sys.argv[2], db=sys.argv[3], charset="utf8")
-    cur = conn.cursor()
+    db = MySQLdb.connect(host="localhost",
+                         port=3306,
+                         user=sys.argv[1],
+                         passwd=sys.argv[2],
+                         db=sys.argv[3])
+    cur = db.cursor()
 
     # Create and execute the query
-    query = "SELECT * FROM states WHERE name = '{}' ORDER BY id ASC"
-    .format(sys.argv[4])
+    # Use format() for creating the SQL query with the user input
+    query = "SELECT * FROM states WHERE name = '{}' ORDER BY id ASC".format(sys.argv[4])
     cur.execute(query)
 
     # Fetch and display the results
-    query_rows = cur.fetchall()
-    for row in query_rows:
+    for row in cur.fetchall():
         print(row)
 
-    # Close cursor and connection
+    # Close all cursors and databases
     cur.close()
-    conn.close()
+    db.close()
+
+if __name__ == "__main__":
+    main()
