8a97148d816fa81c757119146d70245316332188
himza25 <himza.of.theking95@gmail.com>
2023-12-17 12:39:23 -0800

    from task 1 to Task 3

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,20 +1,28 @@
 #!/usr/bin/python3
-"""
-Script that takes in an argument and displays all values in the states
-table of hbtn_0e_0_usa where name matches the argument.
-"""
-
-import MySQLdb
-import sys
+"""Filter states by user input."""
 
 if __name__ == "__main__":
-    db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
-                         passwd=sys.argv[2], db=sys.argv[3])
+    import MySQLdb
+    from sys import argv
+
+    # Connect to the database
+    db = MySQLdb.connect(
+        host="localhost",
+        user=argv[1],
+        passwd=argv[2],
+        db=argv[3]
+    )
     cur = db.cursor()
-    query = "SELECT * FROM states WHERE name = '{}' ORDER BY id ASC"
-    cur.execute(query.format(sys.argv[4]))
-    rows = cur.fetchall()
-    for row in rows:
-        print(row)
+
+    # Execute the query to select states with the name provided by the user
+    query = "SELECT * FROM states WHERE name = %s ORDER BY id ASC"
+    cur.execute(query, (argv[4],))
+    all_rows = cur.fetchall()
+
+    # Print each state that matches the user input
+    for one in all_rows:
+        print(one)
+
+    # Close the cursor and the connection
     cur.close()
     db.close()
