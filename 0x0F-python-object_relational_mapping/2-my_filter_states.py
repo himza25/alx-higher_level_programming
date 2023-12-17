3681fefcda223b37147417bffe76c4c334e0ea02
himza25 <himza.of.theking95@gmail.com>
2023-12-17 12:50:55 -0800

    from task 1 to Task 3

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,36 +1,28 @@
 #!/usr/bin/python3
 """
-A script that takes in an argument and displays all values in the states table
-of hbtn_0e_0_usa where name matches the argument.
+This module contains a script that takes in an argument and displays all values
+in the states table of hbtn_0e_0_usa where name matches the argument.
 """
 
 import MySQLdb
 import sys
 
 if __name__ == "__main__":
-    # Unpack command-line arguments
-    username, password, database, state_name = sys.argv[1:]
-
     # Connect to the database
-    db = MySQLdb.connect(host="localhost", port=3306, user=username,
-                         passwd=password, db=database)
+    conn = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
+                           passwd=sys.argv[2], db=sys.argv[3], charset="utf8")
+    cur = conn.cursor()
 
-    # Create a cursor object
-    cur = db.cursor()
+    # Create and execute the query
+    query = "SELECT * FROM states WHERE name = '{}' ORDER BY id ASC"
+    .format(sys.argv[4])
+    cur.execute(query)
 
-    # Create the SQL query string
-    query = "SELECT * FROM states WHERE name = %s ORDER BY id ASC"
-
-    # Execute the query
-    cur.execute(query, (state_name,))
-
-    # Fetch all the rows
-    rows = cur.fetchall()
-
-    # Print the rows
-    for row in rows:
+    # Fetch and display the results
+    query_rows = cur.fetchall()
+    for row in query_rows:
         print(row)
 
     # Close cursor and connection
     cur.close()
-    db.close()
+    conn.close()
