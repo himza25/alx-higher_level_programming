eec18a80af1c9426c4a51a3f2690e797e510e830
himza25 <himza.of.theking95@gmail.com>
2023-12-17 13:01:40 -0800

    from task 1 to Task 3

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,23 +1,30 @@
 #!/usr/bin/python3
-"""Filter states by name"""
-
-import MySQLdb
-from sys import argv
+"""Filter states by user input"""
 
 if __name__ == "__main__":
-    db = MySQLdb.connect(host="localhost",
-                         user=argv[1],
-                         passwd=argv[2],
-                         db=argv[3])
+    import MySQLdb
+    from sys import argv
+
+    # Connect to the database
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=argv[1],
+        passwd=argv[2],
+        db=argv[3]
+    )
     cur = db.cursor()
 
-    # Safe query execution
-    query = "SELECT * FROM states WHERE name = %s ORDER BY id ASC"
-    cur.execute(query, (argv[4],))
+    # Format the SQL query with the user input
+    query = "SELECT * FROM states WHERE name = '{}' ORDER BY id ASC"
+    cur.execute(query.format(argv[4]))
 
-    # Fetch and display the results
-    for row in cur.fetchall():
-        print(row)
+    # Fetch and print all matching rows
+    all_rows = cur.fetchall()
+    for one in all_rows:
+        if one[1] == argv[4]:
+            print(one)
 
+    # Close the cursor and the connection
     cur.close()
     db.close()
