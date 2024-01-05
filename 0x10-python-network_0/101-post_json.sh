cc61516e299cabe28b0c783b29d16d72250f7f2e
himza25 <himza.of.theking95@gmail.com>
2024-01-05 10:56:48 -0800

    task 8

---
+++ 0x10-python-network_0/101-post_json.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+# This script sends a JSON POST request to a URL with the contents of a specified file and displays the body of the response.
+curl -s -H "Content-Type: application/json" -d @$2 $1
