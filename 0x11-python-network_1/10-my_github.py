1bf2fde077f235d8e070fcae070b9115f29d3ec5
himza25 <himza.of.theking95@gmail.com>
2024-01-06 07:59:38 -0800

    update task 8 and 9

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -1,1 +1,19 @@
 #!/usr/bin/python3
+"""
+Uses the GitHub API with authentication to display user id.
+"""
+
+import requests
+import sys
+
+if __name__ == "__main__":
+    username = sys.argv[1]
+    token = sys.argv[2]
+    url = 'https://api.github.com/user'
+    response = requests.get(url, auth=(username, token))
+
+    try:
+        json_response = response.json()
+        print(json_response.get('id'))
+    except ValueError:
+        print("None")
