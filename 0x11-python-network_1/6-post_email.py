81b048e949c26faa4778747616a071a071f394c5
himza25 <himza.of.theking95@gmail.com>
2024-01-06 07:42:53 -0800

    task 6 to 8

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -1,1 +1,13 @@
 #!/usr/bin/python3
+"""
+Sends a POST request with an email and displays the response body.
+"""
+
+import requests
+import sys
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    email = {'email': sys.argv[2]}
+    response = requests.post(url, data=email)
+    print(response.text)
--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -1,1 +1,15 @@
 #!/usr/bin/python3
+"""
+Sends a request and displays the body of the response or error code.
+"""
+
+import requests
+import sys
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    response = requests.get(url)
+    if response.status_code >= 400:
+        print("Error code:", response.status_code)
+    else:
+        print(response.text)
