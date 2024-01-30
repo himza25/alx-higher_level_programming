590401c276bc0b87e02359eae3f606371f0c88bc
himza25 <himza.of.theking95@gmail.com>
2024-01-30 09:49:33 -0800

    task 6

---
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -0,0 +1,29 @@
+#!/usr/bin/node
+
+const request = require('request');
+
+// Get the API URL from the command line arguments
+const apiUrl = process.argv[2];
+
+request(apiUrl, (error, response, body) => {
+  if (error) {
+    console.error(error);
+    return;
+  }
+
+  const todos = JSON.parse(body);
+  const completedTasks = {};
+
+  todos.forEach(todo => {
+    if (todo.completed) {
+      if (completedTasks[todo.userId]) {
+        completedTasks[todo.userId]++;
+      } else {
+        completedTasks[todo.userId] = 1;
+      }
+    }
+  });
+
+  // Print the number of completed tasks by user ID
+  console.log(completedTasks);
+});
