29229aa02d2bdc83ad222bcf4ccbe03c0a7a979e
himza25 <himza.of.theking95@gmail.com>
2024-01-30 09:06:04 -0800

    task 0

---
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+
+const fs = require('fs');
+
+// Get the file path from the command line arguments
+const filePath = process.argv[2];
+
+// Read the file
+fs.readFile(filePath, 'utf8', (err, data) => {
+    if (err) {
+        // If an error occurred, print the error object
+        console.error(err);
+    } else {
+        // If no error, print the file content
+        console.log(data);
+    }
+});
