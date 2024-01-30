1fa703d9d29bcaf6409bc3f771a289f50401b166
himza25 <himza.of.theking95@gmail.com>
2024-01-30 09:24:37 -0800

    task 0

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -7,11 +7,11 @@ const filePath = process.argv[2];
 
 // Read the file
 fs.readFile(filePath, 'utf8', (err, data) => {
-    if (err) {
-        // If an error occurred, print the error object
-        console.error(err);
-    } else {
-        // If no error, print the file content
-        console.log(data);
-    }
+  if (err) {
+    // If an error occurred, print the error object
+    console.error(err);
+  } else {
+    // If no error, print the file content
+    console.log(data);
+  }
 });
